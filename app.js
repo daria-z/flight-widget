63ce44ac22bb986b6473952b77b60f2c7bafa02a
daria-z <daria.zakirova@gmail.com>
2023-02-15 01:09:11 +0200

    random generated data & interval

--- app.js
+++ app.js
@@ -38,6 +38,17 @@ let flights = [
   },
 ];
 
+const destinations = [
+  "TOKYO",
+  "FRANKFURT",
+  "DUBAI",
+  "LONDON",
+  "OMAN",
+  "BEIRUT",
+];
+const remarks = ["ON TIME", "DELAYED", "CANCELLED"];
+let hour = 15;
+
 function populateTable() {
   for (const flight of flights) {
     const tableRow = document.createElement("tr");
@@ -48,17 +59,70 @@ function populateTable() {
 
       for (const [index, letter] of word.entries()) {
         const letterElement = document.createElement("div");
-
         setTimeout(() => {
           letterElement.classList.add("flip");
           letterElement.textContent = letter;
           tableCell.append(letterElement);
         }, 100 * index);
       }
-
       tableRow.append(tableCell);
     }
     tableBody.append(tableRow);
   }
 }
 populateTable();
+
+function generateRandomLetter() {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+}
+
+function generateRandomNumber(maxNumber) {
+  const numbers = "0123456789";
+  if (maxNumber) {
+    const newNumbers = numbers.slice(0, maxNumber);
+    return newNumbers.charAt(Math.floor(Math.random() * newNumbers.length));
+  } else {
+    return numbers.charAt(Math.floor(Math.random() * numbers.length));
+  }
+}
+
+function generateTime() {
+  let displayHour = hour;
+  if (hour < 24) {
+    hour++;
+  }
+  if (hour >= 24) {
+    hour = 1;
+    displayHour = hour;
+  }
+  if (hour < 10) {
+    displayHour = "0" + hour;
+  }
+  return displayHour + ":" + generateRandomNumber(5) + generateRandomNumber();
+}
+
+function shuffleUp() {
+  flights.shift();
+  flights.push({
+    time: generateTime(),
+    destination: destinations[Math.floor(Math.random() * destinations.length)],
+    flight:
+      generateRandomLetter() +
+      generateRandomLetter() +
+      " " +
+      generateRandomNumber() +
+      generateRandomNumber() +
+      generateRandomNumber(),
+    gate:
+      generateRandomLetter() +
+      " " +
+      generateRandomLetter() +
+      generateRandomLetter(),
+    remarks: remarks[Math.floor(Math.random() * remarks.length)],
+  });
+  tableBody.textContent = "";
+  populateTable();
+}
+
+setInterval(shuffleUp, 3000);
